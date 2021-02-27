e68d943087e50e92cf06179902b4f26c9e3bfc18
Romerof <javierromero.dev@gmail.com>
2021-02-27 00:09:49 -0400

    mejoras al diseño responsive: creado boton de menú tipo hamburguesa; menú lateral desplegable; animaciones y efectos para la el menu; barra superior, suspendida, rediseñada, integracion de todo el menu movil

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
 
+//panel de navegación móvil
 menu.addEventListener("click", (e)=>{
-    e.target.classList.toggle("hide");
+    menu.classList.toggle("hide");
+    logo.classList.toggle("blurred");
+    menu.classList.toggle("tap") && setTimeout(
+        ()=>menu.classList.toggle("tap"), 
+        350
+    );
+    menuBar.classList.toggle("fix")
+
+})
+
+
+//barra de navegacion movil
+document.addEventListener("scroll", (e)=>{
+    barra.classList.toggle("scrolled", window.scrollY > 180) 
 })
